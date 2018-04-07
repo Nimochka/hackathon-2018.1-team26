938bca4c5cde01ffff1c6d5a378563bfe8098a3e
mamirov <m.amirov@gmail.com>
2018-04-07 15:07:07 +0500

    added main method for connect to server from menu

---
+++ Assets/Scripts/UI/ConnectButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class ConnectButton : MonoBehaviour
+    {
+        public InputField NetAddressField;
+
+        public void ConnectToServer()
+        {
+            Debug.Log(NetAddressField.text);
+        }
+    }
+}
