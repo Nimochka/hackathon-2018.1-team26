59147b1c68f78558543dc23778d19f3d4b67beb5
mamirov <m.amirov@gmail.com>
2018-04-08 03:38:43 +0500

    added dash skill for boss

--- Assets/Scripts/Character/Classes/Boss.cs
+++ Assets/Scripts/Character/Classes/Boss.cs
@@ -10,6 +10,8 @@ public class Boss : Character
 {
     [SerializeField] private float moveSpeed;
     public float stunTime = 0;
+    public GameObject dash;
+    public bool destroyMode = false;
     
     protected override void Start()
     {
@@ -33,6 +35,13 @@ public class Boss : Character
         }
     }
 
+    protected override void OnMainSkillUse()
+    {
+        GameObject sDash = Instantiate(dash, transform.position, transform.rotation);
+    }
+    
+    
+
     //protected override void FixedUpdate()
     //{
     //    if (stunTime <= 0)
@@ -46,5 +55,11 @@ public class Boss : Character
     //}
 
 
-    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (destroyMode && other.gameObject.tag == "destroyItems")
+        {
+            GameObject.Destroy(other.gameObject);
+        }
+    }
 }
