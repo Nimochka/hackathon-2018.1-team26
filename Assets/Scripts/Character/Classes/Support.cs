823dd650af164632df11632d7a398cb1c2b16485
mamirov <m.amirov@gmail.com>
2018-04-07 23:15:32 +0500

    added mass team teleport for support

--- Assets/Scripts/Character/Classes/Support.cs
+++ Assets/Scripts/Character/Classes/Support.cs
@@ -18,11 +18,17 @@ public class Support : Character
         MoveSpeed = moveSpeed;
     }
     
+    /**
+     * Хил
+     */
     protected override void OnMainSkillUse()
     {
         Shoot(health);
     }
 
+    /**
+     * Ловушка-стан
+     */
     protected override void OnSecondarySkillUse()
     {
         Vector3 pos = transform.position;
@@ -30,5 +36,28 @@ public class Support : Character
         pos.y += transform.up.y * 20;
         GameObject sBullet = Instantiate(trap, pos, transform.rotation) as GameObject;
         sBullet.GetComponent<Rigidbody2D>().velocity = new Vector2(transform.up.x * 90, transform.up.y * 90);
+    }
+    
+    /**
+     * Масс телепорт к себе
+     */
+    protected override void OnThirdSkillUse()
+    {
+        GameObject tank = GameObject.Find("Tank");
+        GameObject hunter = GameObject.Find("Hunter");
+        if (tank != null)
+        {
+            Teleport(tank);
+        }
+
+        if (hunter != null)
+        {
+            Teleport(hunter);
+        }
+    }
+
+    private void Teleport(GameObject teleportGameObj)
+    {
+        teleportGameObj.transform.position = new Vector3(transform.position.x, transform.position.y);
     }
 }
