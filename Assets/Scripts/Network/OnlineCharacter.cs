201d2f7370d2410b43db20a89ddb18aef6a23126
Artem Konyshev <konyshev_arte@mail.ru>
2018-04-07 16:00:03 +0500

    Added position and rotation sync

---
+++ Assets/Scripts/Network/OnlineCharacter.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class OnlineCharacter : MonoBehaviour
+{
+    private TickData prevTickData, newTickData;
+    private float lerpTimer;
+
+
+    public void ReceiveTick(TickData tickData)
+    {
+        prevTickData = newTickData;
+        newTickData = tickData;
+        lerpTimer = 0;
+    }
+
+
+    void Update()
+    {
+        if (newTickData != null)
+        {
+            if (prevTickData != null)
+            {
+                transform.position = Vector2.Lerp(prevTickData.Position, newTickData.Position,
+                    lerpTimer / Time.fixedDeltaTime);
+                transform.rotation = Quaternion.Euler(Vector3.Lerp(prevTickData.Rotation,
+                    newTickData.Rotation, lerpTimer / Time.fixedDeltaTime));
+                lerpTimer += Time.deltaTime;
+            }
+            else
+            {
+                transform.position = newTickData.Position;
+                transform.rotation = Quaternion.LookRotation(newTickData.Rotation);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Network/SocketController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SocketIO;
+using UnityEngine;
+
+
+public partial class SocketController : MonoBehaviour
+{
+
+    private static SocketIOComponent socket;
+
+    public static string SocketId;
+
+
+    void Start()
+    {
+        socket = GetComponent<SocketIOComponent>();
+        
+        socket.On("open", OnConnected);
+        socket.On("response:player:tick", OnTickReceived);
+
+        socket.Connect();
+    }
+
+
+    public static void RequestPlayerTick(TickData tickData)
+    {
+        socket.Emit("request:player:tick", new JSONObject(JsonUtility.ToJson(tickData)));
+    }
+}
