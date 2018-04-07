c789509c5646735d42bdfd181618ecad1e581818
Artem Konyshev <konyshev_arte@mail.ru>
2018-04-07 21:21:37 +0500

    Added character select

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const io = socketIO(server);
 
 let players_connected = 0;
 
+let pick = {
+    Boss: null,
+    Tank: null,
+    Hunter: null,
+    Support: null,
+};
+
 io.on('connection', socket => {
     ++players_connected;
     if (players_connected <= 4)
@@ -16,16 +23,50 @@ io.on('connection', socket => {
     else
         socket.emit('connect:failure');
 
-    console.log(`player connected ${socket.id}`);
-
+    socket.on('request:player:pick', () => socket.emit('response:character:pick', pick));
     socket.on('request:player:tick', data => socket.broadcast.emit('response:player:tick', data));
     socket.on('request:player:changehealth', data => socket.broadcast.emit('response:player:changehealth', data));
     socket.on('request:player:shot', data => socket.broadcast.emit('response:player:shot', data));
 
+    socket.on('request:character:select', data => pickCharacter(data));
+
     socket.on('disconnect', () => {
         --players_connected;
+        unpickCharacter(socket.id);
     });
 });
+
+function unpickCharacter(socketId) {
+    if (pick.Boss === socketId)
+        pick.Boss = null;
+    else if (pick.Hunter === socketId)
+        pick.Hunter = null;
+    else if (pick.Support === socketId)
+        pick.Support = null;
+    else if (pick.Tank === socketId)
+        pick.Tank = null;
+}
+
+function pickCharacter(data) {
+    unpickCharacter(data.SocketId);
+    if (data.Character === "Boss") {
+        if (pick.Boss == null)
+            pick.Boss = data.SocketId;
+    }
+    else if (data.Character === "Tank") {
+        if (pick.Tank == null)
+            pick.Tank = data.SocketId;
+    }
+    else if (data.Character === "Hunter") {
+        if (pick.Hunter == null)
+            pick.Hunter = data.SocketId;
+    }
+    else if (data.Character === "Support") {
+        if (pick.Support == null)
+            pick.Support = data.SocketId;
+    }
+    io.emit('response:character:pick', pick);
+}
 
 server.listen(3000, function () {
     console.log('Listening on 3000');
