18736fca349955f581ccf3e3a55dd0e98ae46ab7
mamirov <m.amirov@gmail.com>
2018-04-07 20:25:48 +0500

    added hunters skills

--- Assets/Scripts/Character/Classes/Hunter.cs
+++ Assets/Scripts/Character/Classes/Hunter.cs
@@ -18,4 +18,17 @@ public class Hunter : Character
     {
         Shoot(poisonArrow);
     }
+
+    protected override void OnSecondarySkillUse()
+    {
+        GameObject boss = GameObject.Find("Boss");
+        Vector3 bossPosition = boss.transform.position;
+        Vector3 bossForward = boss.transform.up;
+        transform.position = new Vector3(bossPosition.x - (bossForward.x * 30), bossPosition.y - (bossForward.y * 30));
+    }
+
+    protected override void OnThirdSkillUse()
+    {
+        base.OnThirdSkillUse();
+    }
 }
